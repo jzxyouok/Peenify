e0631d28dc2a09b3fd387fb1d1e76d142b9f7b0e
Yish <mombuartworks@gmail.com>
2016-08-30 00:22:43 +0800

    faker data temp

---
+++ database/seeds/CategoriesTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class CategoriesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $categories = ['電影', '遊戲', '動畫'];
+        $user = factory(\App\User::class)->create();
+
+        foreach ($categories as $category) {
+            \App\Category::create([
+                'user_id' => $user->id,
+                'name' => $category,
+                'description' => $category . '分類',
+            ]);
+        }
+    }
+}
---
+++ database/seeds/ProductsTableSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
+
+class ProductsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $categories = \App\Category::all();
+        $faker = Faker::create();
+        $user = factory(\App\User::class)->create();
+
+        foreach ($categories as $category) {
+            for ($i = 1; $i <= random_int(1, 20); $i++) {
+                \App\Product::create([
+                    'category_id' => $category->id,
+                    'user_id' => $user->id,
+                    'name' => $faker->company,
+                    'description' => $faker->sentence,
+                ]);
+            }
+        }
+    }
+}
