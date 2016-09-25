565f3edcc92f9f93d544b316eb17d1f08f0d03b4
Yish <mombuartworks@gmail.com>
2016-09-25 13:46:18 +0800

    update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -38,6 +38,8 @@ class AppServiceProvider extends ServiceProvider
 
         if($this->app->environment() !== 'production') {
             $this->app->register(\Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class);
+            $this->app->register(\Barryvdh\Debugbar\ServiceProvider::class);
+            $this->app->alias('Debugbar', \Barryvdh\Debugbar\Facade::class);
         }
     }
 }
