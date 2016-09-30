0e129e7aa3a2babdbf512ccd9d5a82c12d5926dd
Yish <mombuartworks@gmail.com>
2016-09-30 19:28:36 +0800

    tag

--- tests/Web/TagsControllerTest.php
+++ tests/Web/TagsControllerTest.php
@@ -6,7 +6,7 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class TagsControllerTest extends TestCase
 {
-    use DatabaseMigrations;
+    use DatabaseMigrations, WithoutMiddleware;
 
     public function setUp()
     {
