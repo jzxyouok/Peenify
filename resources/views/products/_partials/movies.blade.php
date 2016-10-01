cb69bcdb4253d773120cf8930d5bc745cd1e59bc
Yish <mombuartworks@gmail.com>
2016-10-01 22:16:49 +0800

    react one

--- resources/views/products/_partials/movies.blade.php
+++
@@ -1,6 +0,0 @@
-@if (!empty($movie->first()))
-<h3>電影額外選項</h3>
-<p>{{ $movie->origin_name }}</p>
-<p>{{ $movie->runtime }}</p>
-<p>{{ $movie->trailer }}</p>
-    @endif
--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -12,28 +12,42 @@
 @section('content')
 
     <div class="container">
+        <!--產品基本項目-->
         <img class="image-size"
              src="{{ ($product->cover) ? image_path('products', $product->cover):'holder.js/400x400' }}">
-
+        <h1>{{ $product->name }}</h1>
+        <p>{{ $product->description }}</p>
         <!--標籤-->
         @include('products._partials.tags', [
         'product' => $product
         ])
 
+        <!------------------------------------->
+
         <!--電影額外選項-->
-        @include('products._partials.movies', [
-            'movie' => $product->movie()
-        ])
+        @if (! empty($product->movie()->first()))
+            <h3>電影額外選項</h3>
+            <p>{{ $product->movie()->origin_name }}</p>
+            <p>{{ $product->movie()->runtime }}</p>
+            <p>{{ $product->movie()->trailer }}</p>
+        @endif
 
-        <h1>{{ $product->name }}</h1>
-        <p>{{ $product->description }}</p>
-
-        <!--需要登入願望清單-->
+        <!--願望清單-->
         @if(auth()->check())
             <div class="form-group">
                 <div id="wish" class="btn btn-{{ $product->existWishByAuth() ? 'danger' : 'default' }}"
                      data-id={{ $product->id }} data-token={{ csrf_token() }}>
                     {{ $product->existWishByAuth() ? '從願望清單移除' : '加到願望清單'}}
+                </div>
+            </div>
+        @endif
+
+        <!--最愛-->
+        @if (auth()->check())
+            <div class="form-group">
+                <div class="favorite btn btn-{{ $product->existFavoriteByAuth() ? 'danger' : 'default' }}"
+                     data-id={{ $product->id }} data-token={{ csrf_token() }}>
+                    {{ $product->existWishByAuth() ? '取消最愛' : '最愛'}}
                 </div>
             </div>
         @endif
@@ -69,22 +83,38 @@
             </div>
         @endif
 
-        @if (auth()->check())
-            <div class="favorite btn btn-{{ $product->existFavoriteByAuth() ? 'danger' : 'default' }}"
-                 data-id={{ $product->id }} data-token={{ csrf_token() }}>
-                {{ $product->existWishByAuth() ? '取消最愛' : '最愛'}}
+        <!--評論表單-->
+        <form action="{{ route('comments.store', $product->id) }}" method="post" role="form">
+            {{ csrf_field() }}
+            <div class="form-group">
+                <label for="description">為 {{ $product->name }} 寫下評論...</label>
+                <textarea name="description" rows="4" cols="50" placeholder="請輸入評論..." class="form-control"></textarea>
             </div>
-        @endif
+            <div class="form-group">
+                <input type="submit" value="評論" class="btn btn-default">
+            </div>
+        </form>
 
-        @include('comments._partials.create', [
-            'product' => $product,
-        ])
+        <!--評論清單-->
+        <div>
+            <h4>Comments</h4>
+            @foreach($product->comments as $comment)
+                <p>{{ $comment->description }}</p>
+                <a href="{{ route('comments.edit', $comment) }}">Edit</a>
 
-        @include('comments._partials.show', [
-            'comments' => $product->comments
-        ])
-    </div>
+                <div id="emoji" class="btn btn-{{ $comment->existEmojiByAuth('like') ? 'danger' : 'default' }}"
+                     data-type="comment" data-emoji="like"
+                     data-id={{ $comment->id }} data-token={{ csrf_token() }}> 喜歡
+                    （{{ $comment->countEmojis('like') }}）
+                </div>
 
+                <div id="emoji" class="btn btn-{{ $comment->existEmojiByAuth('bad') ? 'danger' : 'default' }}"
+                     data-type="comment" data-emoji="bad"
+                     data-id={{ $comment->id }} data-token={{ csrf_token() }}> 不喜歡
+                    （{{ $comment->countEmojis('bad') }}）
+                </div>
+            @endforeach
+        </div>
 @endsection
 
 @section('script')
