0b842ec6e60a7f8bf775801f4e3644baf7cecf5e
Yish <mombuartworks@gmail.com>
2016-10-04 21:27:49 +0800

    nav image

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -64,6 +64,8 @@
                 @else
                     <li class="dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                            <img style="width: 30px;height: 30px;"
+                                 src="{{ (auth()->user()->avatar) ? image_path('avatars.users', auth()->user()->avatar):'holder.js/30x30' }}">
                             {{ Auth::user()->name }} <span class="caret"></span>
                         </a>
 
