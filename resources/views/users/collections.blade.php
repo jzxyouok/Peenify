8694447c4ec603c23153a1a276abc2e43fa87aff
Yish <mombuartworks@gmail.com>
2016-10-25 02:59:39 +0800

    table

--- resources/views/users/collections.blade.php
+++ resources/views/users/collections.blade.php
@@ -1,10 +1,24 @@
 @extends('layouts.app')
 
 @section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 col-md-offset-3">
+                <table class="table table-bordered">
+                    <th>
+                        Name
+                    </th>
+                    @foreach($collections as $collection)
+                        <tr>
+                            <td>
+                                <a href="{{ route('collections.show', $collection->id) }}">{{ $collection->name }}</a>
+                            </td>
+                        </tr>
+                    @endforeach
+                </table>
+            </div>
+        </div>
 
-    @foreach($collections as $collection)
-        <span>{{ $collection->name }}</span>
-    @endforeach
-
-    {!! $collections->links() !!}
+        {!! $collections->links() !!}
+    </div>
 @endsection
