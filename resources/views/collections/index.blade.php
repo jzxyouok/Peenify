b269e49d1521e1c319951e150290f1232a7db42b
Yish <mombuartworks@gmail.com>
2016-10-26 01:13:17 +0800

    collection

--- resources/views/collections/index.blade.php
+++ resources/views/collections/index.blade.php
@@ -1,19 +1,13 @@
 @extends('layouts.app')
 
 @section('style')
-    <link rel="stylesheet" href="{{ asset('/css/card-style.css') }}">
+    <link rel="stylesheet" href="{{ asset('/css/card.css') }}">
+    <link rel="stylesheet" href="{{ asset('/css/area.css') }}">
+    <link rel="stylesheet" href="{{ asset('/css/func.css') }}">
 
     <style>
-        .Card__panel__collection {
-            margin-bottom: 20px;
-            background-color: #fff;
-            -webkit-box-shadow: 0 1px 1px rgba(0, 0, 0, .05);
-            box-shadow: 0 1px 1px rgba(0, 0, 0, .05);
-        }
-
-        .Collection__title {
-            display: inline-block;
-            margin-left: 10px;
+        .Card__panel {
+            height: auto;
         }
 
         .Collection_description {
@@ -29,74 +23,54 @@
 @endsection
 
 @section('content')
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12 text-center" style="padding-bottom: 20px">
-                <h2 class="Card__category__name">
-                    為您精選收藏集
-                </h2>
-            </div>
-
-            <div class="row">
-                @foreach($collections as $collection)
-                    <div class="col-xs-12 col-sm-8 col-md-4 col-lg-4">
-                        <div class="Card__panel__collection" style="border: 1px solid #ccc">
-                            <h3 class="Collection__title">
-                                <a class="Card__title__link"
-                                   href="{{ route('collections.show', $collection->id) }}">{{ str_limit($collection->name, 20) }}
-                                </a>
-                            </h3>
-
-                            <div id="favorite" class="glyphicon glyphicon-heart{{ $collection->isFavorite(auth()->user()) ? ' Favorite__heart__color' : '-empty' }}"
-                                 data-type="collection" data-id={{ $collection->id }} data-token={{ csrf_token() }}>
-                                    <span id="favorite_amount">
-                                        {{ $collection->favorites()->count() }}
-                                    </span>
-                            </div>
-
-                                <div class="Card__detail">
-                                    <div class="Collection_description">
-                                        {{ str_limit($collection->description, 20) }}
-                                    </div>
-
-                                    <div class="Card__count">
-                                        {{ $collection->products()->count() }}
-                                        <span class="Card__count__description">產品</span>
-                                    </div>
-                                </div>
-                        </div>
-                    </div>
-                @endforeach
-            </div>
-
-            <div style="text-align: center">
-                {!! $collections->links() !!}
-            </div>
+<div class="container">
+    <div class="row">
+        <div class="col-md-12 text-center slogan__distance">
+            <h1 class="slogan">
+                探索收藏集
+            </h1>
         </div>
     </div>
+
+    <div class="row">
+        @foreach($collections as $collection)
+            <div class="col-xs-12 col-sm-8 col-md-4 col-lg-4">
+                <div class="Card__panel">
+                    <h3 class="Card__collection__title">
+                        <a class="Card__title__link"
+                           href="{{ route('collections.show', $collection->id) }}">{{ str_limit($collection->name, 20) }}
+                        </a>
+                    </h3>
+
+                    <div id="favorite"
+                         class="glyphicon glyphicon-heart{{ $collection->isFavorite(auth()->user()) ? ' favorite__color' : '-empty' }}"
+                         data-type="collection" data-id={{ $collection->id }} data-token={{ csrf_token() }}>
+                                <span id="favorite_amount">
+                                    {{ $collection->favorites()->count() }}
+                                </span>
+                    </div>
+
+                    <div class="Card__detail">
+                        <div class="Collection_description">
+                            {{ str_limit($collection->description, 20) }}
+                        </div>
+
+                        <div class="Card__count">
+                            {{ $collection->products()->count() }}
+                            <span class="Card__count__description">產品</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        @endforeach
+    </div>
+
+    <div class="text-center">
+        {!! $collections->links() !!}
+    </div>
+</div>
 @endsection
 
 @section('script')
-    <script>
-        $(document).ready(function () {
-            $(document).on('click', '#favorite', function () {
-                var $this = $(this);
-                var $amount = parseInt($this.find('#favorite_amount').text());
-                var token = $this.data('token');
-                var type = $this.data('type');
-                var id = $this.data('id');
-                $.post('/favorites/' + type + '/' + id, {
-                    '_token': token
-                }, function (result) {
-                    if (result.status == 'favorite') {
-                        $this.addClass('glyphicon-heart').addClass('Favorite__heart__color').removeClass('glyphicon-heart-empty');
-                        $this.find('#favorite_amount').html($amount + 1);
-                    } else {
-                        $this.addClass('glyphicon-heart-empty').removeClass('Favorite__heart__color').removeClass('glyphicon-heart');
-                        $this.find('#favorite_amount').html($amount - 1);
-                    }
-                });
-            });
-        });
-    </script>
+    <script src="{{ asset('/js/favorite.js') }}"></script>
 @endsection
