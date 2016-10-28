bf638a99f17709bbaab82c7b2f3bea3bdf73e937
Yish <mombuartworks@gmail.com>
2016-10-28 19:27:18 +0800

    s1

--- resources/views/products/collections.blade.php
+++ resources/views/products/collections.blade.php
@@ -5,27 +5,24 @@
     <div class="container">
         <div class="row">
             @if ($collections->findByUser(auth()->user()->id)->isEmpty())
-                還沒有收藏集嗎？
-                <div class="form-group">
-                    <a class="btn btn-default" href="{{ route('collections.create') }}">建立收藏集</a>
+                <div class="text-center" style="padding-top: 200px">
+                    還沒有收藏集嗎？
+                    <div class="form-group">
+                        <a class="btn btn-default" href="{{ route('collections.create') }}">建立收藏集</a>
+                    </div>
                 </div>
             @else
                 <div class="text-center">
-                    <div class="col-xs-12 col-md-12">
-                        <form class="form-group" action="{{ route('collections.storeProduct', $product_id) }}" method="post">
-                            {{ csrf_field() }}
-                            <div class="col-xs-8 col-md-10">
-                                <select class="form-control" name="collection_id">
-                                    @foreach($collections->findByUser(auth()->user()->id) as $collection)
-                                        <option value="{{ $collection->id }}">{{ $collection->name }}</option>
-                                    @endforeach
-                                </select>
-                            </div>
-                            <div class="col-xs-4 col-md-2">
-                                <input class="btn btn-default" type="submit" value="加入到此收藏集">
-                            </div>
-                        </form>
-                    </div>
+                    <form class="form-group" action="{{ route('collections.storeProduct', $product_id) }}"
+                          method="post">
+                        {{ csrf_field() }}
+                        <select class="form-control" name="collection_id">
+                            @foreach($collections->findByUser(auth()->user()->id) as $collection)
+                                <option value="{{ $collection->id }}">{{ $collection->name }}</option>
+                            @endforeach
+                        </select>
+                        <input class="btn btn-default" type="submit" value="加入到此收藏集">
+                    </form>
                 </div>
             @endif
         </div>
