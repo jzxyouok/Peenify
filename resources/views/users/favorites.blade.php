ab89eb75c0cb095dea8c0d97928312bb605be833
Yish <mombuartworks@gmail.com>
2016-11-02 18:05:10 +0800

    fav

--- resources/views/users/favorites.blade.php
+++ resources/views/users/favorites.blade.php
@@ -22,36 +22,42 @@
         <div class="row">
             @foreach($favorites as $favorite)
                 <div class="col-xs-12 col-sm-8 col-md-4 col-lg-4">
-                    <div class="Card__panel">
-                        <a href="{{ route('products.show', $favorite->favorable->id) }}">
-                            <img class="Card__image"
-                                 src="{{ ($favorite->favorable->cover) ? image_path('products', $favorite->favorable->cover):'holder.js/380x260?auto=yes' }}">
-                        </a>
+                    @if($favorite->favorable instanceof \App\Collection)
+                        <div class="Card__panel">
+                            {{ $favorite->favorable->name }}
+                        </div>
+                    @else
+                        <div class="Card__panel">
+                            <a href="{{ route('products.show', $favorite->favorable->id) }}">
+                                <img class="Card__image"
+                                     src="{{ ($favorite->favorable->cover) ? image_path('products', $favorite->favorable->cover):'holder.js/380x260?auto=yes' }}">
+                            </a>
 
-                        <div>
-                            <div class="Card__detail">
-                                <h3 class="Card__title">
-                                    <a class="Card__title__link"
-                                       href="{{ route('products.show', $favorite->favorable->id) }}">{{ str_limit($favorite->favorable->name, 20) }}
-                                    </a>
-                                </h3>
+                            <div>
+                                <div class="Card__detail">
+                                    <h3 class="Card__title">
+                                        <a class="Card__title__link"
+                                           href="{{ route('products.show', $favorite->favorable->id) }}">{{ str_limit($favorite->favorable->name, 20) }}
+                                        </a>
+                                    </h3>
 
-                                @if(auth()->check() && $favorite->owns())
-                                    <div class="Card__count">
-                                        <div id="favorite"
-                                             class="glyphicon glyphicon-heart{{ $favorite->favorable->isFavorite(auth()->user()) ? ' favorite__color' : '-empty' }}"
-                                             data-type="product"
-                                             data-id={{ $favorite->favorable->id }} data-token={{ csrf_token() }}>
-                                        </div>
+                                    @if(auth()->check() && $favorite->owns())
+                                        <div class="Card__count">
+                                            <div id="favorite"
+                                                 class="glyphicon glyphicon-heart{{ $favorite->favorable->isFavorite(auth()->user()) ? ' favorite__color' : '-empty' }}"
+                                                 data-type="product"
+                                                 data-id={{ $favorite->favorable->id }} data-token={{ csrf_token() }}>
+                                            </div>
 
-                                        <span class="Card__count__description">
+                                            <span class="Card__count__description">
                                             {{ $favorite->created_at->diffForHumans() }}
                                         </span>
-                                    </div>
-                                @endif
+                                        </div>
+                                    @endif
+                                </div>
                             </div>
                         </div>
-                    </div>
+                    @endif
                 </div>
             @endforeach
         </div>
