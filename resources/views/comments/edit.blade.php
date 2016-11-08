30b021f6ce2e3a63b9cf6b35498bca0b73cd5322
Yish <mombuartworks@gmail.com>
2016-11-08 23:41:03 +0800

    edit vue

--- resources/views/comments/edit.blade.php
+++ resources/views/comments/edit.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-    <div class="container">
+    <div class="container" id="app">
         <h1>編輯評論</h1>
 
         <form action="{{ route('comments.update', $comment->id) }}" method="post" role="form" enctype="multipart/form-data">
@@ -11,7 +11,8 @@
             <div class="form-group">
                 <label for="description">類別描述</label>
                 <textarea name="description" rows="4" cols="50"
-                          class="form-control">{{ old('description') ?? $comment->description }}</textarea>
+                          class="form-control" v-model="comment"></textarea>
+                <span>剩餘 @{{ comment_surplus }} 字</span>
             </div>
             <div class="form-group">
                 <input type="hidden" name="product_id" value="{{ $product_id }}">
@@ -20,4 +21,20 @@
         </form>
     </div>
 
+@endsection
+
+@section('script')
+    <script>
+        var app = new Vue({
+            el: '#app',
+            data: {
+                comment: "<?php echo old('description') ?? $comment->description; ?>"
+            },
+            computed: {
+                comment_surplus: function () {
+                    return 200 - this.comment.length
+                }
+            }
+        })
+    </script>
 @endsection
